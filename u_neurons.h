7a281c0ec3a7abf481049f63312ee1109310128c
servomouse <servomouse@protonmail.com>
2023-01-18 21:49:32 +0300

    code
    some cleaning

--- u_neurons.h
+++ u_neurons.h
@@ -5,9 +5,22 @@
 #include <stdio.h>
 
 // Define whether to use float or double
-#define FLOAT float // double
+#define FLOAT float
+// #define FLOAT double
 
-FLOAT get_random(void);
+
 void create_network(int n_layers, ...);
+
+
 void train_network(FLOAT * inputs, FLOAT * outputs, FLOAT coefficient);
+
+// calculate the outputs array for the provied inputs array
 void get_outputs(FLOAT * inputs, FLOAT * outputs);
+
+ // returns either -1 in case of error or 0 if the network stored successfully
+int store_network(char * filename);
+
+ // returns either -1 in case of error or 0 if the network restored successfully
+int restore_network(char * filename);
+
+
