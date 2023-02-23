4e9c4932daa7efeb7cab45e9e45e01fb9165e694
servomouse <servomouse@protonmail.com>
2023-02-23 22:37:26 +0300

    Works with sine

--- main.c
+++ main.c
@@ -8,12 +8,27 @@
 
 #define CREATE
 
+void get_values(float *ins, float *expects)
+{
+    // ins[0] = get_random();
+    // ins[1] = get_random();
+
+    // if(ins[0] > ins[1])
+    //     expects[0] = 1;
+    // else
+    //     expects[0] = -1;
+    float base = rand();
+    ins[0] = sin(base);
+    ins[1] = sin(base+0.01);
+    expects[0] = sin(base+0.02);
+}
+
 #ifdef CREATE
 int main(void)  // create, train and store
 {
     srand((unsigned int)time(NULL));
-    // void * net = create_network(3, 2, 2, 1);
-    void * net = restore_network("network.dat");
+    void * net = create_network(3, 2, 3, 1);
+    // void * net = restore_network("network.dat");
     float ins[2];
     float outs[1];
     float expects[1];
@@ -22,13 +37,7 @@ int main(void)  // create, train and store
     
     for(int i=0; i<200001; i++)
     {
-        ins[0] = get_random();
-        ins[1] = get_random();
-
-        if(ins[0] > ins[1])
-            expects[0] = 1;
-        else
-            expects[0] = -1;
+        get_values(ins, expects);
 
         train_network(net, ins, expects);
 
@@ -36,7 +45,7 @@ int main(void)  // create, train and store
         {
             if(i> 0)
             {
-                update_network(net, 0.1);
+                update_network(net, 0.001);
                 clear_network(net);
             }
 
@@ -44,19 +53,20 @@ int main(void)  // create, train and store
             int error_count = 0;
             for(int j=0; j<1000; j++)
             {
-                ins[0] = get_random();
-                ins[1] = get_random();
-
-                if(ins[0] > ins[1])
-                    expects[0] = 1;
-                else
-                    expects[0] = -1;
+                get_values(ins, expects);
 
                 get_outputs(ins, outs, net);
                 error += pow(expects[0] - outs[0], 2);
                 error_count ++;
             }
-            printf("output: %f,\taverage error: %f\n", outs[0], error/error_count);
+            printf("inputs: %f, %f,", ins[0], ins[1]);
+            printf("\texpects: %f", expects[0]);
+            printf("\toutput: %f,", outs[0]);
+            printf("\taverage error: %f\n", error/error_count);
+            // printf("output: %f,\taverage error: %f\n", outs[0], error/error_count);
+
+            if(error/error_count < 0.01)
+                break;
         }
     }
 
